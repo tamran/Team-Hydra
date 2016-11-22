b0d02901e7f1c60cab5489d002bd6a1a3b1f7b1f
tamran <tamramn@gmail.com>
2016-11-22 11:31:06 -0500

    new current sensor data

--- GeneratorControl/GeneratorControl.ino
+++ GeneratorControl/GeneratorControl.ino
@@ -16,8 +16,8 @@ Servo escServo;
 const double CHOKE_MIN_PL = 0.650; // closed choke, min air
 const double CHOKE_MAX_PL = 1.200; // open choke, max air
  
-const double THROTTLE_MIN_PL = 1.250; // open/full throttle, max gas
-const double THROTTLE_MAX_PL = 1.925; // closed/min throttle, min gas
+const double THROTTLE_MIN_PL = 1.250; // closed (originally open/full throttle, max gas)
+const double THROTTLE_MAX_PL = 1.925; // open   (closed/min throttle, min gas)
 
 const double ESC_MIN_PL = 1;
 const double ESC_MAX_PL = 2;
@@ -32,18 +32,114 @@ const double ESC_MAX_PL = 2;
 const double CHOKE_MIN = 0;
 const double CHOKE_MAX = 100;
 
-const double THROTTLE_MIN = 100;
-const double THROTTLE_MAX = 0;
+const double THROTTLE_MIN = 0;
+const double THROTTLE_MAX = 100;
 
 const double ESC_MIN = -100;
 const double ESC_MAX = 100;
 
 
 // DEFINE SERIAL INPUT
-int percentThrottle = 0;
+int percent = 0;
 double pulseLength = 1500;
 char serialString[100];
 int MODE = 0;
+
+
+
+
+// maps input value to output value for given input range and output range
+double mapVal(double val, double fromMin, double fromMax, double toMin, double toMax){
+  double out = 0;
+  if (val < fromMin){ val = fromMin; }
+  if (val > fromMax){ val = fromMax; }
+  out = (((toMax-toMin)/(fromMax-fromMin))*(val-fromMin)) + toMin;
+  return out; 
+}
+
+
+// sends pwm pulse based on decimal percentage of throttle/amount of openness
+// if ESC, percent = how fast motor is spinning
+// if CHOKE SERVO, percent  = how open it is
+// if THROTTLE SERVO, percent = how closed it is
+void sendPulse(int servoNum, double percent){
+  double minPL,maxPL, minPercent, maxPercent;  
+  Servo myServo;
+  if (servoNum == CHOKE_PIN){
+    minPL = CHOKE_MIN_PL;
+    maxPL = CHOKE_MAX_PL;
+    minPercent = CHOKE_MIN;
+    maxPercent = CHOKE_MAX;
+    myServo = chokeServo;
+  }
+  else if (servoNum == THROTTLE_PIN){
+    minPL = THROTTLE_MIN_PL;
+    maxPL = THROTTLE_MAX_PL;
+    minPercent = THROTTLE_MIN;
+    maxPercent = THROTTLE_MAX;
+    myServo = throttleServo;
+  }
+  else if (servoNum == ESC_PIN){
+    minPL = ESC_MIN_PL;
+    maxPL = ESC_MAX_PL;    
+    minPercent = ESC_MIN;
+    maxPercent = ESC_MAX;
+    myServo = escServo;
+  }
+
+  double pulseLength = mapVal(percent,minPercent,maxPercent,minPL*pow(10,3),maxPL*pow(10,3));
+  myServo.writeMicroseconds(pulseLength);
+
+}
+
+// send servo to min position
+// if ESC, motor off
+// if CHOKE SERVO, closes
+// if THROTTLE SERVO, opens
+void setServoLow(int servoNum){
+ double minPL;
+ Servo myServo;
+ if (servoNum == CHOKE_PIN){
+    minPL = CHOKE_MIN_PL;
+    myServo = chokeServo;
+  }
+  else if (servoNum == THROTTLE_PIN){
+    minPL = THROTTLE_MIN_PL;
+    myServo = throttleServo;
+  }
+  else if (servoNum == ESC_PIN){
+    minPL = ESC_MIN_PL;
+    myServo = escServo;
+  }
+
+  myServo.writeMicroseconds(minPL*pow(10,3));
+  
+}
+
+
+// send servo to max position
+// if ESC, motor at max rpm
+// if CHOKE SERVO, opens
+// if THROTTLE SERVO, closes
+void setServoHigh(int servoNum){
+  double maxPL;
+  Servo myServo;
+ if (servoNum == CHOKE_PIN){
+    maxPL = CHOKE_MAX_PL;
+    myServo = chokeServo;
+  }
+  else if (servoNum == THROTTLE_PIN){
+    maxPL = THROTTLE_MAX_PL;
+    myServo = throttleServo;
+  }
+  else if (servoNum == ESC_PIN){
+    maxPL = ESC_MAX_PL;
+    myServo = escServo;
+  }
+
+  myServo.writeMicroseconds(maxPL*pow(10,3));
+  
+}
 
 
 // initializes ESC PWM
@@ -55,16 +151,26 @@ void initializeESC(){
   Serial.println("Starting Phase 2 - sending 1.65 ms pulse");
   escServo.writeMicroseconds(1650);
   delay(5000);
+   Serial.println("Starting Phase 3 - sending 1.8 ms pulse");
+  escServo.writeMicroseconds(1800);
+  delay(10000);
+  Serial.println("Starting Final Phase 4 - sending 2.0 ms pulse");
+  escServo.writeMicroseconds(2000);
+  delay(5000);
+  // stop after initialization
+  Serial.println("Done Initializing!");
+  escServo.writeMicroseconds(1500);
+  /*
   Serial.println("Starting Phase 3 - sending 1.8 ms pulse");
   escServo.writeMicroseconds(1800);
   delay(10000);
   Serial.println("Starting Final Phase 4 - sending 2.0 ms pulse");
-  Serial.println("Done Initiating" );
   escServo.writeMicroseconds(2000);
   delay(5000);
   // stop after initialization
-  Serial.println("Motor Stopped, ready to go!");
+  Serial.println("Done Initializing!");
   escServo.writeMicroseconds(1500);
+  */
 
 }
 
@@ -86,97 +192,20 @@ void setup() {
 
   initializeESC();
 
-  
-  percentThrottle = 100;
-  delay(2000);
+  /*sendPulse(THROTTLE_PIN,50);
+  setServoHigh(ESC_PIN);
+  Serial.println("Motor Idling at Max Speed");
+  delay(5000);
+  */
+
+
+  setServoLow(ESC_PIN);
+  Serial.println("Motor Stopped");
   
   yield();
 }
 
 
-
-// maps input value to output value for given input range and output range
-double mapVal(double val, double fromMin, double fromMax, double toMin, double toMax){
-  double out = 0;
-  if (val < fromMin){ val = fromMin; }
-  if (val > fromMax){ val = fromMax; }
-  out = (((toMax-toMin)/(fromMax-fromMin))*(val-fromMin)) + toMin;
-  return out; 
-}
-
-
-// sends pwm pulse based on decimal percentage of throttle 
-void sendPulse(int servoNum, double percentThrottle){
-  double minPL,maxPL, minThrottle, maxThrottle;
-  Servo myServo;
-  if (servoNum == CHOKE_PIN){
-    minPL = CHOKE_MIN_PL;
-    maxPL = CHOKE_MAX_PL;
-    minThrottle = CHOKE_MIN;
-    maxThrottle = CHOKE_MAX;
-    myServo = chokeServo;
-  }
-  else if (servoNum == THROTTLE_PIN){
-    minPL = THROTTLE_MIN_PL;
-    maxPL = THROTTLE_MAX_PL;
-    minThrottle = THROTTLE_MIN;
-    maxThrottle = THROTTLE_MAX;
-    myServo = throttleServo;
-  }
-  else if (servoNum == ESC_PIN){
-    minPL = ESC_MIN_PL;
-    maxPL = ESC_MAX_PL;    
-    minThrottle = ESC_MIN;
-    maxThrottle = ESC_MAX;
-    myServo = escServo;
-  }
-
-  double pulseLength = mapVal(percentThrottle,minThrottle,maxThrottle,minPL*pow(10,3),maxPL*pow(10,3));
-  myServo.writeMicroseconds(pulseLength);
-
-}
-
-// send servo to min position/closed configuration
-void setServoLow(int servoNum){
-  double closedPL;
-  Servo myServo;
- if (servoNum == CHOKE_PIN){
-    closedPL = CHOKE_MIN_PL;
-    myServo = chokeServo;
-  }
-  else if (servoNum == THROTTLE_PIN){
-    closedPL = THROTTLE_MAX_PL;
-    myServo = throttleServo;
-  }
-  else if (servoNum == ESC_PIN){
-    closedPL = ESC_MIN_PL;
-    myServo = escServo;
-  }
-
-  myServo.writeMicroseconds(closedPL*pow(10,3));
-  
-}
-
-// send servo to max/open position
-void setServoHigh(int servoNum){
-  double maxPL;
-  Servo myServo;
- if (servoNum == CHOKE_PIN){
-    maxPL = CHOKE_MAX_PL;
-    myServo = chokeServo;
-  }
-  else if (servoNum == THROTTLE_PIN){
-    maxPL = THROTTLE_MIN_PL;
-    myServo = throttleServo;
-  }
-  else if (servoNum == ESC_PIN){
-    maxPL = ESC_MAX_PL;
-    myServo = escServo;
-  }
-
-  myServo.writeMicroseconds(maxPL*pow(10,3));
-  
-}
 
  void loop() {
     /*
